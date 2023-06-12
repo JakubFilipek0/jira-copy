24f98f05bf7a566fbb974436f42670e88795c1cf
JakubFilipek0 <filijaku@gmail.com>
2023-06-12 10:35:44 +0200

    token exp time na 1 miesiąc

--- src/main/java/com/example/testloginregister/config/JwtService.java
+++ src/main/java/com/example/testloginregister/config/JwtService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,12 +34,16 @@ public class JwtService {
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.MONTH, 1);
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+//                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(calendar.getTime())
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
